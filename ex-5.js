3156d01c353dbdf316eb4cdc679f76ed22dfd69f
Sun <zacksunapollo@gmail.com>
2025-04-12 11:03:28 +0700

    Add: Answer to all exercise

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalValue = 27000;
+let discount = (totalValue)*0.25;
+let totalPrice = (totalValue)-(discount);
+console.log (totalPrice);
--- ex-6.js
+++ ex-6.js
@@ -2,5 +2,7 @@
 
 let totalParts = 13000;
 
-let concatSentence;
-let literalSentence;
+let concatSentence = "Toal: "+(totalParts)+" Machine parts";
+let literalSentence = `Total: ${totalParts} Machine parts`;
+console.log (literalSentence);
+console.log (concatSentence);
